af0666d17d73a8ba6138678c2f1d3ada5970012d
Samner Zumaeta <samnerzumaetaescobar3@gmail.com>
2023-10-24 14:57:40 -0500

    commit

--- RESERVAPP/api/routes/hotels.js
+++ RESERVAPP/api/routes/hotels.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { createError } from "../utils/error.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -16,9 +15,10 @@ router.put("/:id", verifyAdmin, updateHotel
 router.delete("/:id",verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
 
 export default router;
