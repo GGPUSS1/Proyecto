22878307e02a024f716b9bc530496ebd94fc2b0b
Samner Zumaeta <samnerzumaetaescobar3@gmail.com>
2023-10-24 14:57:26 -0500

    commit

--- RESERVAPP/reserva/src/App.jsx
+++ RESERVAPP/reserva/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
+import List from './pages/list/List'
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/' element={<Home />} />
+        <Route path='/hotels' element={<List />} />
       </Routes>
     </BrowserRouter>
   )
